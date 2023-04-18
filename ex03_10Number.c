79608d3c1d63349cf245219e03867214aad31367
Tzeng811321 <119690573+Tzeng811321@users.noreply.github.com>
2023-04-18 09:40:30 +0800

    Update ex03_10Number.c

--- ex03_10Number.c
+++ ex03_10Number.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 
-int main()
-{
-    int a, b, c, d, count=0;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int sum;
-    sum = a - b;
-    while (1)
-    {
-        sum -= c;
-        count++;
-        if (sum <= 0 ) break;
-        sum += d;
+int cmp(const void *a, const void *b) {
+    return *(int *)a - *(int *)b;
+}
+
+int main() {
+    int nums[10];
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &nums[i]);
     }
-    
-    printf("%d", count);
+
+    qsort(nums, 10, sizeof(int), cmp);
+
+    int evens[10], odds[10];
+    int even_count = 0, odd_count = 0;
+
+    for (int i = 0; i < 10; i++) {
+        if (nums[i] % 2 == 0) {
+            evens[even_count++] = nums[i];
+        } else {
+            odds[odd_count++] = nums[i];
+        }
+    }
+
+    qsort(evens, even_count, sizeof(int), cmp);
+    qsort(odds, odd_count, sizeof(int), cmp);
+    for (int i = 0; i < even_count; i++) {
+        printf(" %d", evens[i]);
+    }
+    for (int i = odd_count - 1; i >= 0; i--) {
+        printf(" %d", odds[i]);
+    }
+    printf("\n");
+
     return 0;
 }
